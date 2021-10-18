2deb55132c72e408278f529eaf0ce6def206788c
coderonfleek <fikfikky@gmail.com>
2021-10-18 16:05:59 +0100

    First Commit

---
+++ routes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+
+const router = express.Router();
+
+router.get("/", function(req, res) {
+  res.send("Welcome to the Webhooks API");
+});
+
+router.post("/log-circleci-hook", async function(req, res) {
+  //console.log(req.body);
+
+  const payload = req.body;
+
+  let webhook_info = {
+    name : payload.webhook.name,
+    project : payload.project.name,
+    workflow : payload.workflow.name,
+    status : payload.workflow.status
+  }
+
+  const save_webhook = await req.db
+  .collection("webhooks")
+  .insertOne(webhook_info);
+
+  res.status(201).send({
+    message: "Webhook Event successfully logged"
+  });
+});
+
+router.get("/fetch-webhooks-logs", async function(req, res) {
+  console.log(req.body);
+  
+  const webhooks = await req.db
+  .collection("webhooks")
+  .find()
+  .toArray();
+
+  res.status(200).send(webhooks);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const routes = require("./routes");
+const bodyParser = require('body-parser');
+const { startDatabase } = require("./database");
+const crypto = require('crypto');
+
+// App
+const app = express();
+
+const sigHeaderName = 'circleci-signature';
+const sigHashAlg = 'sha256';
+const secret = "XXX-XXX";
+
+
+app.use(bodyParser.json(
+    {
+        verify: (req, res, buf, encoding) => {
+            if (buf && buf.length) {
+                req.rawBody = buf.toString(encoding || 'utf8');
+            }
+        },
+    }
+));
+
+
+// Set port
+const port = process.env.PORT || "1337";
+app.set("port", port);
+
+//Database Setup
+const dbSetup = async (req, res, next) => {
+    if (!req.db) {
+        const db = await startDatabase();
+        req.db = db;
+    }
+
+    next();
+};
+
+app.use(dbSetup);
+
+//Validate payload
+function validatePayload(req, res, next) {
+
+    if(req.method == "POST"){
+        if (!req.rawBody) {
+            return next('Request body empty')
+        }
+    
+        const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8')
+        const hmac = crypto.createHmac(sigHashAlg, secret)
+        const digest = Buffer.from('v1=' + hmac.update(req.rawBody).digest('hex'), 'utf8');
+    
+        if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+            return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`)
+        }
+    }
+
+    return next()
+
+}
+app.use(validatePayload);
+app.use('/', routes);
+
+// Server
+app.listen(port, () => console.log(`Server running on localhost:${port}`));
